696575f151e27108112cb2599e31517ed0db80ba
Johnson <jo74705@gmail.com>
2019-12-25 22:18:57 +0800

    init app

---
+++ src/app.ts
@@ -0,0 +1,30 @@
+import express, {Application, Request, Response, NextFunction} from "express";
+
+const app: Application = express();
+
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response): object => {
+        return res.json({status: "success", message: "Welcome to API Service"});
+    }
+);
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+    const error = new Error("Route Not found");
+    next(error);
+});
+
+app.use((error: { message: string; status: number }, req: Request, res: Response, next: NextFunction
+    ) => {
+        res.status(error.status || 500);
+        res.json({
+            status: "error",
+            message: error.message
+        });
+        next();
+    }
+);
+
+const PORT: any = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`app listening on port ${PORT}`));
